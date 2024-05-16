1904b7d5bf3f51cc9ab977fa839e6e2f856f0711
J. R. Petrus <jrpetrus@gmail.com>
2024-05-15 21:29:01 -0600

    Compiles and runs with both Icarus and Verilator.

--- test/test.py
+++ test/test.py
@@ -3,38 +3,48 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
+
+
+def set_packed_bit(dut_word, bit_idx, bit_val):
+    val = int(dut_word.value)
+    val |= (bit_val & 0x1) << bit_idx
+    dut_word.value = val
+
+
+def get_packed_bit(dut_word, idx):
+    return (int(dut_word.value) >> idx) & 0x1
 
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    dut.spi.sck.value = 0
+    dut.spi.cs_n.value = 1
+    dut.spi.sdi.value = 0
+    dut.gpio.gpi.value = 0
+    await Timer(10, "ns")
+
+    # Set the clock period to 20 ns (50 MHz)
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    set_packed_bit(dut.gpio.gpi, 13, 1)
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # # Set the input values you want to test
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert True
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
