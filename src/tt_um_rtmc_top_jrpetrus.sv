989b2d182058d050601d670cc5d4774744e10058
J. R. Petrus <jrpetrus@gmail.com>
2024-05-27 13:08:23 -0600

    Fix SPI pinout to utilize RP2040 SPI0.

--- src/tt_um_rtmc_top_jrpetrus.sv
+++ src/tt_um_rtmc_top_jrpetrus.sv
@@ -5,6 +5,38 @@
 
 `default_nettype none
 
+/*
+Inputs
+ui[0]: "General Purpose Input gpi[0]"
+ui[1]: "General Purpose Input gpi[1]"
+ui[2]: "General Purpose Input gpi[2]"
+ui[3]: "General Purpose Input gpi[3]"
+ui[4]: "SPI0.cs"
+ui[5]: "SPI0.sck"
+ui[6]: "SPI0.tx"
+ui[7]: "Connected to uo[6]"
+
+Outputs
+uo[0]: "General Purpose Output gpo[0]"
+uo[1]: "General Purpose Output gpo[1]"
+uo[2]: "General Purpose Output gpo[2]"
+uo[3]: "General Purpose Output gpo[3]"
+uo[4]: "Connected to ^uio_in"
+uo[5]: "Connected to ui[7]"
+uo[6]: "Connected to ena"
+uo[7]: "SPI0.rx"
+
+Bidirectional pins
+uio[0]: "Motor Control mc[0]"
+uio[1]: "Motor Control mc[1]"
+uio[2]: "Motor Control mc[2]"
+uio[3]: "Motor Control mc[3]"
+uio[4]: "Motor Control mc[4]"
+uio[5]: "Motor Control mc[5]"
+uio[6]: "Motor Control mc[6]"
+uio[7]: "Motor Control mc[7]"
+*/
+
 module tt_um_rtmc_top_jrpetrus (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -16,12 +48,14 @@ module tt_um_rtmc_top_jrpetrus (
     input  wire       rst_n     // reset_n - low to reset
 );
     // Tie-off and prevent lint warnings.
-    assign uo_out[6:4] = {ena, ui_in[4], ^uio_in};
+    assign uo_out[6] = ena;
+    assign uo_out[5] = ui_in[7];
+    assign uo_out[4] = ^uio_in;
 
     rtmc_core core(
         .sck(ui_in[5]),
-        .cs_n(ui_in[6]),
-        .sdi(ui_in[7]),
+        .cs_n(ui_in[4]),
+        .sdi(ui_in[6]),
         .sdo(uo_out[7]),
         .gpi({ui_in[3:0]}),
         .gpo(uo_out[3:0]),
--- test/rtmc_tb.sv
+++ test/rtmc_tb.sv
@@ -25,13 +25,45 @@ module rtmc_tb();
   logic [2:0] uo_dont_care;
   logic ena;
 
+  /*
+  Inputs
+  ui[0]: "General Purpose Input gpi[0]"
+  ui[1]: "General Purpose Input gpi[1]"
+  ui[2]: "General Purpose Input gpi[2]"
+  ui[3]: "General Purpose Input gpi[3]"
+  ui[4]: "SPI0.cs"
+  ui[5]: "SPI0.sck"
+  ui[6]: "SPI0.tx"
+  ui[7]: "Connected to uo[6]"
+
+  Outputs
+  uo[0]: "General Purpose Output gpo[0]"
+  uo[1]: "General Purpose Output gpo[1]"
+  uo[2]: "General Purpose Output gpo[2]"
+  uo[3]: "General Purpose Output gpo[3]"
+  uo[4]: "Connected to ^uio_in"
+  uo[5]: "Connected to ui[7]"
+  uo[6]: "Connected to ena"
+  uo[7]: "SPI0.rx"
+
+  Bidirectional pins
+  uio[0]: "Motor Control mc[0]"
+  uio[1]: "Motor Control mc[1]"
+  uio[2]: "Motor Control mc[2]"
+  uio[3]: "Motor Control mc[3]"
+  uio[4]: "Motor Control mc[4]"
+  uio[5]: "Motor Control mc[5]"
+  uio[6]: "Motor Control mc[6]"
+  uio[7]: "Motor Control mc[7]"
+  */
+
   tt_um_rtmc_top_jrpetrus dut(
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
-      .ui_in({spi.mosi, spi.cs, spi.sclk, 1'b0, gpio.gpi}),
+      .ui_in({1'b0, spi.mosi, spi.sclk, spi.cs, gpio.gpi}),
       .uo_out({spi.miso, uo_dont_care, gpio.gpo}),
       .uio_in(8'd0),
       .uio_out(motor.mc),
